40415d89fa7f9676375bb3eecd2a7700be2a852c
Gene De Lisa <gene@rockhoppertech.com>
2012-06-26 08:07:41 -0400

    Initial Commit

---
+++ GraphWithSoundsTests/GraphWithSoundsTests.h
@@ -0,0 +1,13 @@
+//
+//  GraphWithSoundsTests.h
+//  GraphWithSoundsTests
+//
+//  Created by Gene De Lisa on 6/26/12.
+//  Copyright (c) 2012 Rockhopper Technologies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GraphWithSoundsTests : SenTestCase
+
+@end
---
+++ GraphWithSoundsTests/GraphWithSoundsTests.m
@@ -0,0 +1,32 @@
+//
+//  GraphWithSoundsTests.m
+//  GraphWithSoundsTests
+//
+//  Created by Gene De Lisa on 6/26/12.
+//  Copyright (c) 2012 Rockhopper Technologies. All rights reserved.
+//
+
+#import "GraphWithSoundsTests.h"
+
+@implementation GraphWithSoundsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GraphWithSoundsTests");
+}
+
+@end
